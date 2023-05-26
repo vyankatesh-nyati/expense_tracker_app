3861709b41e61f09bde47290ba9735c0d062d10d
vyankatesh <vrnyati@gmail.com>
2023-05-26 22:43:47 +0530

    appbar for app

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -28,6 +28,15 @@ class _ExpensesScreenState extends State<ExpensesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expense Tracker"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
       body: Column(
         children: [
           const Text("chart"),
