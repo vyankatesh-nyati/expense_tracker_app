bf1bac74eafbb427412463a482904f2c1acbfd0c
vyankatesh <vrnyati@gmail.com>
2023-05-27 23:27:34 +0530

    theme setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
+final kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(const MyApp());
 }
@@ -8,14 +12,36 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Expense Tracker',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData().copyWith(
         useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.onSecondary,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: const TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 18,
+                color: kColorScheme.onSecondaryContainer,
+              ),
+            ),
       ),
       debugShowCheckedModeBanner: false,
       home: const ExpensesScreen(),
